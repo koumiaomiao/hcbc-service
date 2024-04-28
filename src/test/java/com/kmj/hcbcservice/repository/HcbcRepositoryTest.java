43f4bce51d9936c3610f02c8c77c2360ae0ead37
mjkou <mjkou@thoughtworks.com>
2024-04-28 08:50:31 +0800

    [Feature] add findById test method

--- src/test/java/com/kmj/hcbcservice/repository/HcbcRepositoryTest.java
+++ src/test/java/com/kmj/hcbcservice/repository/HcbcRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.kmj.hcbcservice.repository;
 
 import com.kmj.hcbcservice.document.Book;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,10 +20,20 @@ public class HcbcRepositoryTest {
 
     private Book book;
 
-    @Test
-    public void should_insert_a_book_to_repository_success_when_create_a_book() {
+    @BeforeEach
+    public void setUp() {
         book = new Book(1, "title1", "author1", "1998", "123456");
         hcbcRepository.save(book);
+    }
+
+
+    @Test
+    public void should_insert_a_book_to_repository_success_when_create_a_book() {
         assertThat(hcbcRepository.findAll()).isNotEmpty();
+    }
+
+    @Test
+    public void should_get_the_book_when_find_book_by_id() {
+        hcbcRepository.findById(String.valueOf(book.getId())).ifPresent(value -> assertThat(value).isEqualTo(book));
     }
 }
